ad509d96038688a3298a3dbd060384e6070c1c59
Edghi <ghislain.makolle@facsciences-uy1.cm>
2022-04-27 15:49:54 +0100

    Adding the last contexts on the index page

--- comsas_juin/core/views.py
+++ comsas_juin/core/views.py
@@ -1,8 +1,7 @@
 from datetime import datetime
-from django.http import QueryDict
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from .models import Event, Speaker
+from .models import Event, Partner, Slider, Speaker
 
 
 class ScheduleView(TemplateView):
@@ -13,13 +12,19 @@ def index(request):
     """
     Return the page view with the contexts of the data.
     """
+    # Speakers context data
     speakers = Speaker.objects.all()
+    # Programs/Events context data
     day_1 = Event.objects.all().filter(date=datetime(2022, 6, 6))
     day_2 = Event.objects.all().filter(date=datetime(2022, 6, 7))
     day_3 = Event.objects.all().filter(date=datetime(2022, 6, 8))
     day_4 = Event.objects.all().filter(date=datetime(2022, 6, 9))
     day_5 = Event.objects.all().filter(date=datetime(2022, 6, 10))
     day_6 = Event.objects.all().filter(date=datetime(2022, 6, 11))
+    # Sliders context data
+    sliders = Slider.objects.all()
+    # Partners context data
+    partners = Partner.objects.all()
     return render(request,
                   "core/index.html",
                   context={
@@ -30,6 +35,8 @@ def index(request):
                       'day_4': day_4,
                       'day_5': day_5,
                       'day_6': day_6,
+                      'sliders': sliders,
+                      'partners': partners,
                   })
 
 
