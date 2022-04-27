87740a885697497ecf667a2d1c37d437e017b832
Edghi <ghislain.makolle@facsciences-uy1.cm>
2022-04-27 15:29:17 +0100

    Add of the context of speakers and programs on the index page

--- comsas_juin/core/urls.py
+++ comsas_juin/core/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from core.views import IndexView, SpeakerView, ContactView, ScheduleView, AboutView
+from core.views import SpeakerView, ContactView, ScheduleView, AboutView, index
 app_name = 'core'
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='home'),
+    path('', index, name='home'),
     path('speakers', SpeakerView.as_view(), name='speakers'),
     path('contact', ContactView.as_view(), name='contact'),
     path('schedule', ScheduleView.as_view(), name='schedule'),
--- comsas_juin/core/views.py
+++ comsas_juin/core/views.py
@@ -1,13 +1,36 @@
+from datetime import datetime
+from django.http import QueryDict
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import Event, Speaker
 
 
 class ScheduleView(TemplateView):
     template_name = "core/schedule.html"
 
 
-class IndexView(TemplateView):
-    template_name = "core/index.html"
+def index(request):
+    """
+    Return the page view with the contexts of the data.
+    """
+    speakers = Speaker.objects.all()
+    day_1 = Event.objects.all().filter(date=datetime(2022, 6, 6))
+    day_2 = Event.objects.all().filter(date=datetime(2022, 6, 7))
+    day_3 = Event.objects.all().filter(date=datetime(2022, 6, 8))
+    day_4 = Event.objects.all().filter(date=datetime(2022, 6, 9))
+    day_5 = Event.objects.all().filter(date=datetime(2022, 6, 10))
+    day_6 = Event.objects.all().filter(date=datetime(2022, 6, 11))
+    return render(request,
+                  "core/index.html",
+                  context={
+                      'speakers': speakers,
+                      'day_1': day_1,
+                      'day_2': day_2,
+                      'day_3': day_3,
+                      'day_4': day_4,
+                      'day_5': day_5,
+                      'day_6': day_6,
+                  })
 
 
 class AboutView(TemplateView):
@@ -16,6 +39,7 @@ class AboutView(TemplateView):
 
 class SpeakerView(TemplateView):
     template_name = "core/speaker.html"
+
 
 class ContactView(TemplateView):
     template_name = "core/contact.html"
